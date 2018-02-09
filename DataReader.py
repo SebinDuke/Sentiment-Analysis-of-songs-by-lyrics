31fa9855a5d01cfea82003b78907978533f0be4f
SebinDuke <sebindukestephan@gmail.com>
2018-02-09 17:31:17 +0530

    init.py and correction to generic Data Reader

--- DataReader.py
+++ DataReader.py
@@ -1,18 +1,19 @@
 import re
 
-def ReadData(path,emotion):
+def readData(path,emotion):
     Data=[]
     lyrics=[]
     for i in range(1,101):
         filename=path+emotion+"_"+str(i)+".txt"
-        with open(filename,'r') as file:
+        with open(filename,'r',encoding='latin-1') as file:
             lyric=file.read()
             (lyric,count)=re.subn(r"(\\n|\\u....|\t)","",lyric)
             (lyric,count)=re.subn(r"(\[\d\d:\d\d\.\d\d\])","",lyric)
             lyrics.append(lyric)
 
 
-    with open(path+"info.txt",'r') as file:
+    #with codecs.open(path+"info.txt", mode='r', errors='ignore') as file:
+    with open(path+"info.txt",'r',encoding='latin-1') as file:
         i=0
         for line in file:
             row=line.split(':')
@@ -22,6 +23,8 @@ def ReadData(path,emotion):
 
     return Data
 
-Data=ReadData("Data-Set/Angry/Train/","angry")
+"""
+Data=readData("Data-Set/Angry/Train/","angry")
 for i in Data:
     print(i)
+"""
