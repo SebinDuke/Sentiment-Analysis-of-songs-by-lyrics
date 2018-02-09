e81ef9680e75ff622bb923213df011c01baba6ac
SebinDuke <sebindukestephan@gmail.com>
2018-02-09 16:45:40 +0530

    Generic Data Reader

--- DataReader.py
+++ DataReader.py
@@ -1,24 +1,27 @@
 import re
 
-angry=[]
-lyrics=[]
-path="Data-Set/Angry/Train/"
-for i in range(1,101):
-    filename=path+"angry_"+str(i)+".txt"
-    with open(filename,'r') as file:
-        lyric=file.read()
-        (lyric,count)=re.subn(r"(\\n|\\u....|\t)","",lyric)
-        (lyric,count)=re.subn(r"(\[\d\d:\d\d\.\d\d\])","",lyric)
-        lyrics.append(lyric)
+def ReadData(path,emotion):
+    Data=[]
+    lyrics=[]
+    for i in range(1,101):
+        filename=path+emotion+"_"+str(i)+".txt"
+        with open(filename,'r') as file:
+            lyric=file.read()
+            (lyric,count)=re.subn(r"(\\n|\\u....|\t)","",lyric)
+            (lyric,count)=re.subn(r"(\[\d\d:\d\d\.\d\d\])","",lyric)
+            lyrics.append(lyric)
 
 
-with open(path+"info.txt",'r') as file:
-    i=0
-    for line in file:
-        row=line.split(':')
-        row.append(lyrics[i])
-        angry.append(row)
+    with open(path+"info.txt",'r') as file:
+        i=0
+        for line in file:
+            row=line.split(':')
+            row.append(lyrics[i])
+            Data.append(row)
+            i+=1
 
+    return Data
 
-for i in angry:
+Data=ReadData("Data-Set/Angry/Train/","angry")
+for i in Data:
     print(i)
